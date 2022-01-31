d4baf639374707c7e7597d60fb0ef460fad0919c
york <york11122@gmail.com>
2022-01-31 13:45:34 +0800

    fix bug

--- deploy-command.js
+++ deploy-command.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
+require('dotenv').config();
 
 const commands = [
     new SlashCommandBuilder().setName('alert').setDescription('gas fee 追蹤提醒').addIntegerOption(option => option.setName('gwei')
@@ -10,7 +11,7 @@ const commands = [
 ]
     .map(command => command.toJSON());
 
-const rest = new REST({ version: '9' }).setToken("OTM2NjI5ODk0Nzk0ODU0NDUw.YfP-kA.Pe_R6mjBK9uhHkxewRdMzvDG5MY");
+const rest = new REST({ version: '9' }).setToken(process.env.DISCORD_TOKEN);
 
 rest.put(Routes.applicationCommands("936629894794854450"), { body: commands })
     .then(() => console.log('Successfully registered application commands.'))
